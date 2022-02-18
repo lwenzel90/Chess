aa084977962343f98423f831c3a82fbfe43b139c
Logan <lwenzel90@gmail.com>
2022-02-18 18:58:59 -0500

    main file is now chess.cpp, so remove main.cpp. board.hpp updates from adding piece

--- src/board.hpp
+++ src/board.hpp
@@ -8,11 +8,52 @@
 
 class Board {
  public:
-    explicit Board(sf::RenderWindow* window);
-    std::vector<sf::RectangleShape*> mBoardSquares;
-
+   explicit Board(sf::RenderWindow* window);
+   std::vector<sf::RectangleShape*> mBoardSquares;
+   bool draw(sf::RenderWindow* window);
  private:
-    double mSquareLen;
+   float mSquareLen;
+   sf::Image wKingImage; 
+   sf::Texture wKingTexture;
+   sf::Sprite wKingSprite;
+   sf::Image bKingImage; 
+   sf::Texture bKingTexture;
+   sf::Sprite bKingSprite;
+
+   sf::Image wQueenImage; 
+   sf::Texture wQueenTexture;
+   sf::Sprite wQueenSprite;
+   sf::Image bQueenImage; 
+   sf::Texture bQueenTexture;
+   sf::Sprite bQueenSprite;
+   
+   sf::Image wRookImage; 
+   sf::Texture wRookTexture;
+   sf::Sprite wRookSprite;
+   sf::Image bRookImage; 
+   sf::Texture bRookTexture;
+   sf::Sprite bRookSprite;
+   
+   sf::Image wBishopImage; 
+   sf::Texture wBishopTexture;
+   sf::Sprite wBishopSprite;
+   sf::Image bBishopImage; 
+   sf::Texture bBishopTexture;
+   sf::Sprite bBishopSprite;
+   
+   sf::Image wKnightImage; 
+   sf::Texture wKnightTexture;
+   sf::Sprite wKnightSprite;
+   sf::Image bKnightImage; 
+   sf::Texture bKnightTexture;
+   sf::Sprite bKnightSprite;
+
+   sf::Image wPawnImage; 
+   sf::Texture wPawnTexture;
+   sf::Sprite wPawnSprite;
+   sf::Image bPawnImage; 
+   sf::Texture bPawnTexture;
+   sf::Sprite bPawnSprite;
 };
 
 
